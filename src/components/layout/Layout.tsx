017ee7a71017bac1887e46f1ca8a8800df693319
Jo-shua09 <joshuaonyeka039@gmail.com>
2026-01-06 21:17:41 -0800

    `Update Layout component to add max-width and margin auto`

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col max-w-[185rem] mx-auto">
       <Header />
       <main className="flex-1 pt-20 overflow-hidden">{children}</main>
       <Footer />
