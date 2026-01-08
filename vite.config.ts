f78384d09c1040effcf3a5926f9b25b9b570bb78
Jo-shua09 <joshuaonyeka039@gmail.com>
2026-01-08 13:01:57 -0800

    `Update vite.config.ts with new server configuration and dependencies optimization`

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: { port: 8080 },
+  server: {
+    port: 8080,
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  optimizeDeps: {
+    include: ["react-helmet-async"], // ensures react-helmet-async works with Vite
   },
 });
