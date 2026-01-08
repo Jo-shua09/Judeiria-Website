d56639c40f66cf6d0a1f73c71eb41cf2fda785dd
Jo-shua09 <joshuaonyeka039@gmail.com>
2026-01-08 12:41:55 -0800

    `Added BrowserRouter and conditional rendering for dev environment`

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import AppRouter from "./routes/AppRouter";
 import Loader from "./components/Loader";
+import { BrowserRouter } from "react-router-dom";
+
+const isDev = import.meta.env.DEV;
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,5 +16,17 @@ export default function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  return <div>{isLoading ? <Loader /> : <AppRouter />}</div>;
+  const router = <AppRouter />;
+
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : isDev ? (
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>{router}</BrowserRouter>
+      ) : (
+        router
+      )}
+    </div>
+  );
 }
